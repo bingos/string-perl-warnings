6ea31d7ed48a1914e951ec1f4ecce9ee6be3cf54
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-09-30 13:23:42 +0100

    Remove the other \c type warning too

--- lib/String/Perl/Warnings.pm
+++ lib/String/Perl/Warnings.pm
@@ -947,7 +947,7 @@ q{\"\\B\{\"\ is\ deprecated\;\ use\ \"\\B\\\{\"\ instead},
 q{\"\\B\{\"\ is\ deprecated\;\ use\ \"\\B\\\{\"\ or\ \"\\B\[\{\]\"\ instead\ in\ regex\;\ marked\ by\ \<\-\-\ HERE\ in\ m\/.+?\/},
 q{\"\\b\{\"\ is\ deprecated\;\ use\ \"\\b\\\{\"\ instead},
 q{\"\\b\{\"\ is\ deprecated\;\ use\ \"\\b\\\{\"\ or\ \"\\b\[\{\]\"\ instead\ in\ regex\;\ marked\ by\ \<\-\-\ HERE\ in\ m\/.+?\/},
-q{\"\\c.+?\"\ is\ more\ clearly\ written\ simply\ as\ \".+?\"},
+#q{\"\\c.+?\"\ is\ more\ clearly\ written\ simply\ as\ \".+?\"},
 #q{\"\\c\{\"\ is\ deprecated\ and\ is\ more\ clearly\ written\ as\ \"\;\"}, # seriously this is nonsense
 q{\"my\ .+?\"\ used\ in\ sort\ comparison},
 q{\"my\ sub\"\ not\ yet\ implemented},
